ef00251784937bb33184c8c991b3751bf9e58da6
Saikath Bhattacharya <saikath1@users.noreply.github.com>
2022-10-13 12:46:12 -0500

    New codes

---
+++ src/BaseballPlayerDriver.java
@@ -0,0 +1,9 @@
+public class BaseballPlayerDriver {
+    public static void main(String[] args) {
+        BaseballPlayer sam;
+        sam = new BaseballPlayer(10, "ss");
+        sam.printPlayer();
+    }
+}
+
+
