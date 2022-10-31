997b7a24d1584d381a79fbb267b730b53af56149
saikath1 <saikath.bhattacharya@gmail.com>
2022-10-31 14:34:14 -0500

    Week567 package updated

--- src/main/java/BaseballPlayerDriver.java
+++
@@ -1,9 +0,0 @@
-public class BaseballPlayerDriver {
-    public static void main(String[] args) {
-        BaseballPlayer sam;
-        sam = new BaseballPlayer(10, "ss");
-        sam.printPlayer();
-    }
-}
-
-
